6f3b81cba9152c84c2d752db77005601d13608d1
Ayush <aadarsha0997@gmail.com>
2025-11-01 13:44:52 +0545

    changing background makeing ui professional

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -250,9 +250,9 @@ export default function Home() {
   )
 
   return (
-    <div className="min-h-screen bg-white max-w-full mx-auto ">
+    <div className="bg-momondo-purple w-full min-h-full">
       {/* Header */}
-      <header className="border-b border-gray-200 sticky top-0 bg-white z-50">
+      <header className="border-b border-white/20 sticky top-0 bg-momondo-purple z-50">
         <div className="px-8 py-4 flex items-center justify-between">
           <div className="flex items-center gap-12">
             <div className="text-pink-500 font-bold text-2xl">airbnb</div>
@@ -327,7 +327,7 @@ export default function Home() {
       </main>
 
       {/* Footer */}
-      <footer className="bg-gray-100 border-t border-gray-200 py-12">
+      <footer className="bg-momondo-purple border-t border-white/20 py-12 text-white">
         <div className="px-8 max-w-7xl mx-auto">
           <div className="grid grid-cols-4 gap-8 mb-8">
             <div>
